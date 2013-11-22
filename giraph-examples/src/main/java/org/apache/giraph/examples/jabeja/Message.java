44896ac7abd4cf3a58896f69b417c2dec71d34c4
Peter Grman <peter.grman@gmail.com>
2013-11-22 04:22:17 +0100

    Exchange colors to be able to calculate the lokal energy of each node

---
+++ giraph-examples/src/main/java/org/apache/giraph/examples/jabeja/Message.java
@@ -0,0 +1,11 @@
+package org.apache.giraph.examples.jabeja;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: peter
+ * Date: 11/22/13
+ * Time: 1:55 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Message {
+}
