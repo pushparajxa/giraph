e09e3cadc633dff25995c3caf7427a63c79e1825
pushparajxa <pushparajxa@gmail.com>
2013-12-24 01:33:06 +0530

    Rands table size

--- giraph-examples/src/main/java/org/apache/giraph/examples/jabeja/NodePartitioningComputation.java
+++ giraph-examples/src/main/java/org/apache/giraph/examples/jabeja/NodePartitioningComputation.java
@@ -70,6 +70,7 @@ public class NodePartitioningComputation
       this.rnds.put(Long.valueOf(vertex.getId().get()), new Random(vertex
           .getId().get()));
     }
+    System.out.println("The size of rands table is= " + rnds.size());
 
     if (isTimeToStop()) {
       this.vertex.voteToHalt();
